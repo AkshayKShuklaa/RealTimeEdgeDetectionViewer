1cb515a665f4e4ee2a5b03d00c1b52b464ab009c
shukl <2k22.csaiml.32598@gmail.com>
2025-11-13 23:20:35 +0530

    sdk.main2

---
+++ sdk/build/intermediates/cxx/Debug/2q6p4nn5/logs/arm64-v8a/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\shukl\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "D:\\Real Time Edge Detection Viewer\\sdk\\.cxx\\Debug\\2q6p4nn5\\arm64-v8a" ^
+  opencv_jni_shared
